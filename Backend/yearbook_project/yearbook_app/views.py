60ee24c5e19cdc154d74911b2dd7031c39f5e222
nikhil-iitb <200010050@iitb.ac.in>
2022-02-11 19:49:47 +0530

    registration, getting error while sending registration request from react

--- Backend/yearbook_project/yearbook_app/views.py
+++ Backend/yearbook_project/yearbook_app/views.py
@@ -53,6 +53,8 @@ def register(request) :
         request.session['otp'] = otp
         request.session['otp_email'] = email
         request.session.save()
+        return Response({"status": "OTP sent successfully to your LDAP Id. It is `${otp}`"})
+
         html_content = "<p> Otp for login is: <b>" + f"{otp}" + "</b></p>"
         # response = send_mail(email,name,"OTP: YearBook 2021", html_content=html_content,  
         #             sender_email="sarc@iitb.ac.in", sender_name="SARC", reply_name="SARC", reply_to="no-reply-sarc@iitb.ac.in")
@@ -68,10 +70,10 @@ def register(request) :
         #     first_name = req_data['first_name'],
         #     last_name = req_data['last_name']
         #     )
-        return Response({"status": "OTP sent successfully to your LDAP Id. It is `${otp}`"})
     
     submitted_otp = req_data['otp_field']
-    if f"{request.session.get('otp',-1)}" == submitted_otp and request.session.get('otp_email',-1)==email:                    
+    if f"{request.session.get('otp',-1)}" == submitted_otp and request.session.get('otp_email',-1)==email: 
+        print("Correct")                   
         student, created = Student.objects.get_or_create(sso_id=sso)
         if created:
             student.first_name=req_data['first_name']
@@ -83,198 +85,3 @@ def register(request) :
         return Response( { "status" : "registered"})
 
 
-@api_view (['POST'])
-def register1(request):
-    form = LoginForm(request.POST or None)
-    if form.is_valid():
-        sso = str(form.cleaned_data.get('email')).lower()
-        email = sso + "@iitb.ac.in"
-        iitborg = form.cleaned_data.get('iitborg',None)
-        if iitborg is not None and iitborg != '':
-            email_correct = True
-            if Alum.objects.filter(sso_id = sso).exists():
-                alum = Alum.objects.get(sso_id = sso)
-                email_correct = False if ((alum.email).split('@')[0]).lower() != iitborg else email_correct
-            email = str(iitborg).lower() + "@iitbombay.org" if email_correct else None
-            if email is None:
-                return("Need email")
-                # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': f'Alum with this sso and iitbombay.org username does not exist'})
-            
-        if form.cleaned_data.get('otp_field','') == '' or form.cleaned_data.get('otp_field',None) is None:
-            name = form.cleaned_data.get('first_name') + " " + form.cleaned_data.get('last_name')
-            import random
-            otp = random.randint(100000,1000000)
-            request.session['otp'] = otp
-            request.session['otp_email'] = email
-            request.session.save()
-            html_content = "<p> Otp for login is: <b>" + f"{otp}" + "</b></p>"
-            response = send_mail(email,name,"OTP: YearBook 2021", html_content=html_content,  
-                        sender_email="sarc@iitb.ac.in", sender_name="SARC", reply_name="SARC", reply_to="no-reply-sarc@iitb.ac.in")
-            if response.status_code != 202:
-                response_iitb_mail = send_sso_mail(email,name,"OTP: YearBook 2021", html_content=html_content,  
-                        sender_email="sarc@iitb.ac.in", sender_name="SARC", reply_name="SARC", reply_to="no-reply-sarc@iitb.ac.in")
-                # if len(response_iitb_mail.keys())!=0:
-                    # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': 'OTP NOT SENT. Contact Dhairya Jain (9967876281)'})
-            EmailNotifs.objects.create(
-                email = email,
-                otp = otp,
-                first_name = form.cleaned_data.get('first_name'), 
-                last_name = form.cleaned_data.get('last_name')
-            )
-            return Response({"status": "OTP sent successfully to your LDAP Id"})
-                # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': f'OTP Sent Successfully to {email}'})
-        else:
-            submitted_otp = form.cleaned_data.get('otp_field')
-            if f"{request.session.get('otp',-1)}" == submitted_otp and request.session.get('otp_email',-1)==email:                    
-                req_data = request.data
-                student, created = Student.objects.get_or_create(sso_id=req_data['sso_id'])
-                    
-                profile, profile_created = Student_Profile.objects.get_or_create(student = student, email=email)
-                if created:
-                    student.first_name=form.cleaned_data.get('first_name')
-                    student.last_name=form.cleaned_data.get('last_name')
-                    student.save()
-                request.session['user_id'] = student.sso_id
-                request.session.save()
-    return Response( { "status" : "registered"})
-                
-
-
-def index(request):
-    """
-    The person always lands on this page after logging in,
-    the view is divided into three panes,
-    in the first pane, we have the person's photo, some details and various options to navigat through the site
-    it is essentially our nav bar, similar to what we see on facebook
-    the second pane is where a list of recommended friends will be displayed
-    the third pane is where we will have the recent feed, encapsulating who most recently wrote what for someone else
-
-    """
-    # request.session['user_id'] = '193310023'
-    # request.session.save()
-    if request.method == 'GET':
-        try:
-            if request.session.get('user_id',"abc")!="abc":
-                if request.GET.get('r'):
-                    a=request.GET.get('r')
-            else:
-                raise Exception
-        except:
-        
-            if request.GET.get('access_token'):
-            
-                access_token = request.GET.get('access_token')
-                path = "https://gymkhana.iitb.ac.in/sso/user/api/user/?fields=first_name,last_name,type,insti_address,mobile,program,roll_number"
-                http_request = urllib.Request(path)
-                #gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-                http_request.add_header("Authorization", "Bearer "+access_token)
-                result = urllib.urlopen(http_request)
-                data = json.load(result)
-                try:
-                
-                    logged_in_student = Student.objects.get(sso_id = data['roll_number'])
-                    request.session['user_id'] = logged_in_student.sso_id
-                except:
-                
-                    logged_in_student = Student(sso_id = data['roll_number'], first_name = data.get('first_name', '').title(), last_name = data.get('last_name', '').title())
-                    logged_in_student.save()
-                try:
-                
-                    profile = Student_Profile.objects.get(student = logged_in_student)
-                    if profile.details_done:
-                    
-                        if request.GET.get('r'):
-                            
-                            a=request.GET.get('r')
-
-                except:
-                    
-                    profile = Student_Profile(student = logged_in_student)
-                    if data.get('program', None):
-                        program = data['program']
-                        if program.get('department_name', None):
-                            profile.department = program['department_name']
-                        if program.get('join_year', None):
-                            profile.join_year = program['join_year']
-                        if program.get('graduation_year', None):
-                            profile.graduation_year = program['graduation_year']
-                        if program.get('degree', None):
-                            profile.degree = program['degree']
-                    if data.get('insti_address', None):
-                        address = data['insti_address']
-                        if address.get('hostel', None):
-                            profile.hostel = address['hostel']
-                        if address.get('room', None):
-                            profile.room_no = address['room']
-                    if data.get('type', None):
-                        profile.program = data['type']
-                    profile.save()
-                    request.session['user_id'] = logged_in_student.sso_id
-                    print("E13")
-                    if request.GET.get('r'):
-                        print("E14")
-                        a=request.GET.get('r')
-        form = LoginForm(None)
-        if request.GET.get('r'):
-            
-            redirect_uri = request.GET.get('r')
-            return Response({'status': 'Person already created'})
-
-            # return render(request,'index.html',{'redirect_uri':redirect_uri,'redirect':True,'form':form, 'alert': 'Welcome to YearBook21'})
-        # else:
-            
-            # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': 'Welcome to YearBook21'})
-    else:
-        form = LoginForm(request.POST or None)
-        if form.is_valid():
-            sso = str(form.cleaned_data.get('email')).lower()
-            email = sso + "@iitb.ac.in"
-            iitborg = form.cleaned_data.get('iitborg',None)
-            if iitborg is not None and iitborg != '':
-                email_correct = True
-                if Alum.objects.filter(sso_id = sso).exists():
-                    alum = Alum.objects.get(sso_id = sso)
-                    email_correct = False if ((alum.email).split('@')[0]).lower() != iitborg else email_correct
-                email = str(iitborg).lower() + "@iitbombay.org" if email_correct else None
-            # if email is None:
-                # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': f'Alum with this sso and iitbombay.org username does not exist'})
-            
-            if form.cleaned_data.get('otp_field','') == '' or form.cleaned_data.get('otp_field',None) is None:
-                name = form.cleaned_data.get('first_name') + " " + form.cleaned_data.get('last_name')
-                import random
-                otp = random.randint(100000,1000000)
-                request.session['otp'] = otp
-                request.session['otp_email'] = email
-                request.session.save()
-                html_content = "<p> Otp for login is: <b>" + f"{otp}" + "</b></p>"
-                response = send_mail(email,name,"OTP: YearBook 2021", html_content=html_content,  
-                            sender_email="sarc@iitb.ac.in", sender_name="SARC", reply_name="SARC", reply_to="no-reply-sarc@iitb.ac.in")
-                if response.status_code != 202:
-                    response_iitb_mail = send_sso_mail(email,name,"OTP: YearBook 2021", html_content=html_content,  
-                            sender_email="sarc@iitb.ac.in", sender_name="SARC", reply_name="SARC", reply_to="no-reply-sarc@iitb.ac.in")
-                    # if len(response_iitb_mail.keys())!=0:
-                        # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': 'OTP NOT SENT. Contact Dhairya Jain (9967876281)'})
-                EmailNotifs.objects.create(
-                    email = email,
-                    otp = otp,
-                    first_name = form.cleaned_data.get('first_name'), 
-                    last_name = form.cleaned_data.get('last_name')
-                )
-                
-                # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': f'OTP Sent Successfully to {email}'})
-            else:
-                submitted_otp = form.cleaned_data.get('otp_field')
-                if f"{request.session.get('otp',-1)}" == submitted_otp and request.session.get('otp_email',-1)==email:                    
-                    student, created = Student.objects.get_or_create(sso_id=sso)
-                    
-                    profile, profile_created = Student_Profile.objects.get_or_create(student = student, email=email)
-                    if created:
-                        student.first_name=form.cleaned_data.get('first_name')
-                        student.last_name=form.cleaned_data.get('last_name')
-                        student.save()
-                    request.session['user_id'] = student.sso_id
-                    request.session.save()
-
-                    # return redirect('home')
-                # else:
-                    # return render(request, 'index.html',{'redirect':False, 'form': form, 'alert': 'Wrong OTP for this roll no'})
--- Frontend/src/components/pages/Login.js
+++ Frontend/src/components/pages/Login.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./Login.css";
+import "../css/Login.css";
+import "../css/base.css";
+import axios from 'axios';
+
 
 export default function Login() {
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+const [student, setStudent] = useState(
+    {
+        first_name: '',
+        last_name: '',
+        email: '',
+        iitb_org: '',
+        otp_field: ''
+    }
+)
 
 function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return student.email.length > 0
 }
 
+
+
+
+function handleChange(e) {
+    setStudent( {...student, [e.target.name]: e.target.value} )
+}
 function handleSubmit(event) {
     event.preventDefault();
-}
-
+    axios.post("http://127.0.0.1:8000/api/register/", student)
+            .then(function(res){
+                console.log(res)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    }
 return (
     <div className="Login">
         <div className="head">Welcome To The YearBook!
@@ -28,7 +50,7 @@ return (
         <Form.Group size="lg" controlId="firstname">
         <Form.Label>First name:</Form.Label>
         <br />
-        <Form.Control type="text" size="lg"
+        <Form.Control type="text" size="lg" name="first_name" value={student.first_name} onChange={handleChange}
         placeholder="First name" />
         </Form.Group>
         <div className="description">Enter your first name</div>
@@ -36,7 +58,7 @@ return (
         <Form.Label>Last name:</Form.Label>
         <br />
         <Form.Control type="text" 
-        placeholder="Last name " />
+        placeholder="Last name " name="last_name" value={student.last_name} onChange={handleChange} />
         </Form.Group>
         <div className="description">Enter your last name</div>
         <Form.Group size="lg" controlId="email">
@@ -44,10 +66,11 @@ return (
         <br />
         <Form.Control
             autoFocus
-            type="number"
-            placeholder="Eg 200020057 1234567891011121314"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="200020057"
+            value={student.email}
+            name="email"
+            onChange={handleChange}
         />
         </Form.Group>
         <div className="description">Enter your roll number: Email will be sent to roll-no@iitb.ac.in</div>
@@ -56,8 +79,9 @@ return (
         <br/>
         <Form.Control
             type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name="iitb_org"
+            value={student.iitb_org}
+            onChange={handleChange}
         />
         <div className="description">Enter your iitbombay.org username: Email will be sent to username@iitbombay.org.
         Use only if you're an alumni, and don't have email forwarding enabled 
@@ -68,20 +92,24 @@ return (
         <br />
         <Form.Control
             autoFocus
-            type="number"
+            type="text"
             placeholder="Eg 123456 or leave blank to generate an otp"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={student.otp_field}
+            name="otp_field"
+            onChange={handleChange}
         />
         </Form.Group>
         <div className="description">Enter the 6 digit OTP you recieved on your email (roll-no@iitb.ac.in). Leave blank to Generate a new OTP</div>
         <div className="btn">
+        {student.otp_field == '' ? 
         <Button block size="lg" type="submit" disabled={!validateForm()}>
         Generate OTP
         </Button>   
+        :
         <Button block size="lg" type="submit" disabled={!validateForm()}>
-        Generate OTP
+        Submit
         </Button>
+}
         </div>
         <br/>
 
