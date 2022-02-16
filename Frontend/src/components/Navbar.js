81aa741f41dcaf4bd5660ae9d788af7cc21f194d
Manav Doshi <200100094@iitb.ac.in>
2022-02-16 20:30:03 +0530

    Fixing navbar

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,27 +1,56 @@
 import React from 'react'
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import "./css/Navbar.css";
+import { BsFillHouseFill, BsImages, BsBarChartLineFill, BsFillPersonFill, BsFillQuestionCircleFill, BsPower } from "react-icons/bs";
 
 const Navbar_func = () => {
   return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">Yearbook</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+<nav class="navbar navbar-expand-lg navbar-light bg-light main-navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Yearbook</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse middle-links first-links" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 middle-links">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">SARC</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">IIT Bombay</a>
+        </li>
+
+      </ul>
+      <form class="d-flex second-links">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+{/*         <button class="btn btn-outline-success" type="submit">Search</button> */}
+      </form>
+      <div>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 third-links">
+      <li class="nav-item">
+          <a class="nav-link" href="#"><BsFillHouseFill/></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><BsImages/></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><BsBarChartLineFill/></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><BsFillPersonFill/></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><BsFillQuestionCircleFill/></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><BsPower/></a>
+        </li>
+      </ul>
+      </div>
+
+    </div>
+  </div>
+</nav>
 
   
   )
